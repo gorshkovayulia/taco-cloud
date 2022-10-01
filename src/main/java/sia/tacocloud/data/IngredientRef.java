faa90aafe4ab67d0a8249a955cdb6766dbe0183f
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-10-01 20:08:55 +0300

    Add comment

---
+++ src/main/java/sia/tacocloud/data/IngredientRef.java
@@ -0,0 +1,9 @@
+package sia.tacocloud.data;
+
+import lombok.Data;
+
+// Represents the connection between Taco and Ingredient
+@Data
+public class IngredientRef {
+    private final String ingredient;
+}
