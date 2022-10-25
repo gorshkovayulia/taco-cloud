0de90ad9e971ac6420c5cbdba1a5760e36980ef7
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-10-25 15:11:20 +0300

    Add custom queries

--- src/main/java/sia/tacocloud/repository/IngredientRepository.java
+++ src/main/java/sia/tacocloud/repository/IngredientRepository.java
@@ -3,12 +3,5 @@ package sia.tacocloud.repository;
 import org.springframework.data.repository.CrudRepository;
 import sia.tacocloud.data.Ingredient;
 
-import java.util.Optional;
-
 public interface IngredientRepository extends CrudRepository<Ingredient, String> {
-
-    Iterable<Ingredient> findAll();
-    Optional<Ingredient> findById(String id);
-    Ingredient save(Ingredient ingredient);
-
 }
--- src/main/java/sia/tacocloud/repository/OrderRepository.java
+++ src/main/java/sia/tacocloud/repository/OrderRepository.java
@@ -3,7 +3,18 @@ package sia.tacocloud.repository;
 import org.springframework.data.repository.CrudRepository;
 import sia.tacocloud.data.TacoOrder;
 
+import java.util.Date;
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 
-    TacoOrder save(TacoOrder order);
+    /**
+     * Fetches all the orders delivered to a given ZIP code
+     */
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+
+    /**
+     * Queries for all orders delivered to a given ZIP code within a given date range
+     */
+    List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
 }
