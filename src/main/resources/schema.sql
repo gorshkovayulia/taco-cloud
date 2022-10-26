ccd93a249adc7f6c21d2b101dde6514ebcfb149f
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-10-26 20:11:42 +0300

    Fix .sql files

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -28,7 +28,8 @@ create table if not exists Ingredient_Ref (
 create table if not exists Ingredient (
   id varchar(4) not null,
   name varchar(25) not null,
-  type varchar(10) not null
+  type varchar(10) not null,
+  unique (id)
 );
 
 alter table Taco
